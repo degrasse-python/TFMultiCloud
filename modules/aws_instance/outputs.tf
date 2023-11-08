df86b000ee89fb6d8f09d9c89490306badfa34f7
Deon Saunders <51129091+degrasse-python@users.noreply.github.com>
2023-11-08 12:10:26 -0500

    fixed outputs

--- modules/aws_instance/outputs.tf
+++ modules/aws_instance/outputs.tf
@@ -1,11 +1,21 @@
 output "asg_name" {
   description = "Name of the Auto Scaling Group"
-  value       = aws_autoscaling_group.example_asg.name
+  value       = aws_autoscaling_group.web_asg.name
+}
+
+output "asg_target_group_arns" {
+  description = "Target Group ARNs of the Auto Scaling Group"
+  value       = aws_autoscaling_group.web_asg.target_group_arns
+}
+
+output "asg_launch_configuration" {
+  description = "Config of the Auto Scaling Group"
+  value       = aws_autoscaling_group.web_asg.launch_configuration
 }
 
 output "asg_desired_capacity" {
   description = "Desired capacity of the Auto Scaling Group"
-  value       = aws_autoscaling_group.example_asg.desired_capacity
+  value       = aws_autoscaling_group.web_asg.desired_capacity
 }
 
 output "web_nlb_dns_name" {
--- modules/gcp_instance/outputs.tf
+++ modules/gcp_instance/outputs.tf
@@ -1,11 +1,12 @@
 output "instance_names" {
   description = "Names of the VM instances in GCP"
-  value       = google_compute_instance.example_vm[*].name
+  value       = google_compute_instance.example_vm.name
+  
 }
 
 output "instance_external_ips" {
   description = "External IP addresses of the VM instances in GCP"
-  value       = google_compute_instance.example_vm[*].network_interface[0].access_config[0].nat_ip
+  value       = google_compute_instance.example_vm.network_interface[0].access_config[0].nat_ip
 }
 
 output "postgresql_instance_ip" {
@@ -15,5 +16,5 @@ output "postgresql_instance_ip" {
 
 output "gcp_project_id" {
   description = "GCP Project ID"
-  value       = google_project.default.project_id
+  value       = google_compute_instance.example_vm.project
 }
