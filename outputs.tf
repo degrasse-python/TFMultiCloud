9e9e973b3eef99667b4611772f763b2615ffc019
Deon Saunders <51129091+degrasse-python@users.noreply.github.com>
2023-11-08 12:27:01 -0500

    fixed ooo

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "aws_instances_name" {
-  value = module.aws.*.instance_name
+  value = module.aws.asg_name
 }
 
-output "google_instance_name" {
-  value = module.google.*.instance_name
+output "google_instance_ips" {
+  value = module.google.instance_external_ips
 }
 
 
-output "db_instance_name" {
-  value = module.google.*.db_instance_name
+output "db_instance_ip" {
+  value = module.google.postgresql_instance_ip
 }
