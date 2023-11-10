4e36e44e602f7f7e82b40d6d7fcd340aa253564e
Deon Saunders <51129091+degrasse-python@users.noreply.github.com>
2023-11-10 02:38:35 -0500

    fiix hopefully

--- modules/aws_instance/aws_instance.tf
+++ modules/aws_instance/aws_instance.tf
@@ -17,14 +17,12 @@ resource "aws_vpc" "example_vpc" {
 // Pull Rquest 1 - change vpc_id to data.aws_vpc.default.id
 resource "aws_subnet" "example_subnet_1" {
   vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "example_subnet_2" {
   vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
@@ -162,7 +160,7 @@ resource "aws_security_group" "db_sg" {
 resource "aws_db_instance" "example" {
   allocated_storage    = 20
   engine               = "postgres"
-  engine_version       = "12.4"
+  engine_version       = "15.3-R2"
   instance_class       = "db.t2.micro"
   username             = "admin"
   password             = "password"
